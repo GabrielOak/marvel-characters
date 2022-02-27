5a3793ef4ae1367a648a0d3d8bd759dee66342dd
Gabriel Carvalho <gabriel315df@gmail.com>
2022-02-26 21:39:36 -0300

    Fix card test

--- src/components/Card/test.spec.js
+++ src/components/Card/test.spec.js
@@ -2,14 +2,17 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import Card from '.';
 
-const props = {
-  title: 'Spider-man',
-  img: 'https://i.annihil.us/u/prod/marvel/i/mg/e/03/5317713c9e746.jpg',
+const character = {
+  name: 'Spider-man',
+  thumbnail: {
+    path: 'https://i.annihil.us/u/prod/marvel/i/mg/e/03/5317713c9e746',
+    extension: 'jpg',
+  },
 };
 
 describe('<Card />', () => {
   it('should render the card', () => {
-    render(<Card {...props} />);
+    render(<Card content={character} />);
 
     expect(
       screen.getByRole('heading', { name: /spider-man/i })
